793a8eec5fe0e68f4cef39587076cafcc8a9dd54
bdm-k <kokusyunn@gmail.com>
2025-01-18 19:09:51 +0900

    2025-01-18 19:09

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -21,7 +21,7 @@ in
   # environment.
   home.packages = [
     # pkgs.zsh
-    # pkgs.starship
+    pkgs.starship
     # pkgs.fzf
 
     pkgs.ripgrep
@@ -43,14 +43,13 @@ in
     # '')
   ];
 
+  xdg.configFile = {
+    "starship.toml".source = "/home/${username}/resources/starship.toml";
+  };
+
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
@@ -58,14 +57,14 @@ in
     # '';
   };
 
-  # programs.git = {
-  #   enable = true;
-  #   userName = "bdm-k";
-  #   userEmail = "kokusyunn@gmail.com";
-  #   extraConfig = {
-  #     core.editor = "nvim";
-  #   };
-  # };
+  programs.git = {
+    enable = true;
+    userName = "bdm-k";
+    userEmail = "kokusyunn@gmail.com";
+    extraConfig = {
+      core.editor = "nvim";
+    };
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
