9e31ed2b8dbbc2af566f221f9a3eab68aaa955ce
bdm-k <kokusyunn@gmail.com>
2025-07-25 22:15:48 +0900

    nix: install cmake

--- nix/x86_64-linux/home.nix
+++ nix/x86_64-linux/home.nix
@@ -8,22 +8,11 @@ in
     then [ ./desktop.nix ]
     else [];
 
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = username;
   home.homeDirectory = "/home/${username}";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "24.11";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
     pkgs.starship
     pkgs.fzf
@@ -31,6 +20,7 @@ in
     pkgs.neofetch
     pkgs.gcc
     pkgs.gnumake
+    pkgs.cmake
     pkgs.clang-tools
     pkgs.lua-language-server
 
@@ -53,28 +43,7 @@ in
     # my programs
     programs.starship_agnocast_kmod
     programs.safe-rm
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
-
-  home.file = {
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
 
   programs.git = {
     enable = true;
@@ -85,6 +54,5 @@ in
     };
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
